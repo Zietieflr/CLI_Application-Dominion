705c9ab6450f7cc843f1a0a2f3ca3d0ae33ad8d6
hawleywoo <alhawley21@gmail.com>
2020-05-27 21:38:23 -0600

    Add user login.

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,4 +1,4 @@
-class Cli
+class Cli 
   
    def prompt
       TTY::Prompt.new
@@ -13,17 +13,64 @@ class Cli
   end
 
   def user_name 
-    puts "Enter Your User Name: "
-    user_name = get_user_input
-    "Welcome #{user_name}, We're here to help you conquer your Dominion!"
-    ########  check if in database if not add new user
-    if  
-      User.create()
+    new_user = prompt.yes?("Are you a new user? (Y/n)")
+    
+
+    if new_user
+      puts "Create Your New User Name: "
+
+      user_name_input = get_user_input
+
+      user = User.find_by(user_name: user_name_input)
+
+      if user
+        while user
+        puts "Sorry, this name has been taken."
+        puts "Try another name: "
+        user_name_input = get_user_input
+        user = User.find_by(user_name: user_name_input)
+        end
+        puts "Welcome #{user_name_input}, We're here to help you conquer your Dominion!"
+      else
+        User.create(user_name: user_name_input)
+        puts "Welcome #{user_name_input}, We're here to help you conquer your Dominion!"
+      end
+    else
+      puts "Enter your user name: "
+      user_name_input = get_user_input
+      puts "Welcome back #{user_name_input}, We're here to help you conquer your Dominion!"
+      
+    end
+
+
+
+
+
+
+    # puts "Enter Your User Name: "
+    # user_name_input = get_user_input
+    # "Welcome #{user_name_input}, We're here to help you conquer your Dominion!"
+    # ########  check if in database if not add new user
+    # user = User.find_by(user_name: user_name_input) 
+    # # if user
+    # #   puts "User Name is taken" 
+    # # until @user do
+
+    # #  end
   end
 
+
+
+
   def main_menu 
-    choices = %w('Quick Start/ Setup' 'Browse Cards' 'Create Set' 'My Sets' 'My Expansions' 'Exit')
-    prompt.select("Choose Option:", choices)
+    choices = {
+      'Quick Start/Setup' => 1, 
+      'Browse Cards' => 2 ,
+      'Create Set' => 3, 
+      'My Sets' => 4, 
+      'My Expansions' => 5, 
+      'Exit' => 6}
+    answer = prompt.select("Choose Option:", choices, cycle: true)
   end
 
   def start_game
